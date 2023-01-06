0972f81c8925e42958250eeb47ff079437d67d71
Nana1423 <91445608+Nana1423@users.noreply.github.com>
2023-01-06 15:21:31 -0800

    Add files via upload

---
+++ Design Pattern Exercises/AdapterPattern_CoffeeMachine/src/CoffeeMachine/OldCoffeeMachine.java
@@ -0,0 +1,13 @@
+package CoffeeMachine;
+
+class OldCoffeeMachine {
+	
+	public void selectA() {
+		System.out.println("A - Selected");
+	}
+	
+	public void selectB() {
+		System.out.println("B - Selected");
+	}
+	
+}
